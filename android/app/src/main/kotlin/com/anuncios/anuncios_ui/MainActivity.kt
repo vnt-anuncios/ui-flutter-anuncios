da6ed39d1477ea5913f2771650f89e85c00ac435
oswaldo orellana <oswaldo.orellana.v@gmail.com>
2021-07-09 16:05:32 -0400

    initial

---
+++ android/app/src/main/kotlin/com/anuncios/anuncios_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anuncios.anuncios_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Application",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+      ),
+    );
+  }
+}
